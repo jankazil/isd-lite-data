75144f9f354c37dfb8c87699a09b2d0f0d9b5e22
Jan Kazil <jan.kazil.dev@gmail.com>
2025-08-04 14:20:24 -0600

    Adds script for downloading ISDLite data files with observations in the contiguous US (CONUS)
    Adds scripts/Download_CONUS_Stations_Metadata.py
    
    	This script downloads IDSLite station metadata file isd-history.txt and
    filters it:
    
    	- to produce a list of stations in the contiguous US (saved as
    conus_stations.txt)
    	- to produce a list of stations in the contiguous US which have observations
    in the period 2020-12-03 to 2025-07-31 (saved as CONUS.2020-12-03
    00:00:00-2025-07-31 00:00:00.txt)
    
    Adds scripts/Download_CONUS_Stations_Observations.py
    
    	This script reads the list of stations in the contiguous US which have
    observations in the period 2020-12-03 to 2025-07-31 (CONUS.2020-12-03
    00:00:00-2025-07-31 00:00:00.txt) and downloads the ISDLITE data files with the
    corresponding IDSLite observations for these stations and time period.

---
+++ scripts/Download_CONUS_Stations_Metadata.py
@@ -0,0 +1,71 @@
+"""
+
+Downloads IDSLite station metadata and filters the stations to obtain stations
+in the contiguous US which have observations available for download in a specified
+time period.
+
+"""
+
+from pathlib import Path
+from datetime import datetime
+from isd_lite_data import ncei
+from isd_lite_data import data
+
+# Data directory
+
+data_dir = Path('..') / 'data'
+
+#
+# Stations in the contiguous US (CONUS)
+#
+
+# Identify contiguous US (CONUS) stations
+
+all_stations = data.Stations.from_url()
+
+conus_stations = all_stations.filter_by_country(['US'])
+
+min_lat = 25
+max_lat = 50
+min_lon = -125
+max_lon = -65
+
+# Save the stations in the contiguous US (CONUS)
+
+file_name = 'conus_stations.txt'
+
+file_path = data_dir / file_name
+
+conus_stations.save('Stations in the contiguous US (CONUS)',file_path)
+
+#
+# Stations in the contiguous US (CONUS) with data for a given period
+#
+
+# Period of interest
+
+start_date = datetime(2020, 12, 3)
+end_date = datetime(2025, 7, 31)
+
+# Load the stations in the contiguous US (CONUS)
+
+file_name = 'conus_stations.txt'
+
+file_path = data_dir / file_name
+
+conus_stations = data.Stations.from_file(file_path)
+
+# Filter the stations by whether observations are available for download for the given period
+
+conus_stations = conus_stations.filter_by_data_availability(start_date,end_date,n_jobs=32,verbose=True)
+
+# Save the stations in the contiguous US (CONUS) that have observations are available for download for the given period
+
+file_name = 'CONUS.' + str(start_date) + '-' + str(end_date) + '.txt'
+
+file_path = data_dir / file_name
+
+conus_stations.save('Stations in the contiguous US (CONUS), with observations available for download in the period ' + str(start_date) + ' - ' + str(end_date),file_path)
+
+print('Saved file with stations meeting criteria: ',file_path)
+
---
+++ scripts/Download_CONUS_Stations_Observations.py
@@ -0,0 +1,42 @@
+"""
+
+Downloads IDSLite station observations for stations in a given stations database file.
+
+"""
+
+from pathlib import Path
+from datetime import datetime
+from isd_lite_data import ncei
+from isd_lite_data import data
+
+#
+# Download data from stations in the contiguous US (CONUS) with data for a given period
+#
+
+# Data directory
+
+data_dir = Path('..') / 'data'
+
+# Period of interest
+
+start_date = datetime(2020, 12, 3)
+end_date = datetime(2025, 7, 31)
+
+# Load stations in the contiguous US (CONUS)
+
+file_name = 'CONUS.' + str(start_date) + '-' + str(end_date) + '.txt'
+
+file_path = data_dir / file_name
+
+conus_stations = data.Stations.from_file(file_path)
+
+# Download data
+
+local_files = ncei.download_many(
+    start_date.year,
+    end_date.year,
+    conus_stations.id(),
+    data_dir,
+    n_jobs=32,
+    refresh = False,
+    verbose=True)
